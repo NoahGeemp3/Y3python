992e3e6f37a66a46e113dce59819e83509bd0e46
SubaruPagani <7285@trinity.croydon.sch.uk>
2025-03-10 10:44:16 +0000

    Add files via upload

---
+++ variables and input challenge.py
@@ -0,0 +1,11 @@
+name = input("enter your name>>>")
+print("hello", name)
+
+age = input("enter your age>>>")
+print("im also" , age)
+
+favcolour = input("enter your favourite colour>>>")
+print("I also like", favcolour)
+
+favsportsteam = input("enter your favourite sports team>>>")
+print("i like", favsportsteam, "too")
