8e886dd51f5fe45a4ce4789a908a09a3a2a49476
SubaruPagani <7285@trinity.croydon.sch.uk>
2025-03-10 10:31:48 +0000

    Add files via upload

---
+++ variables explained.py
@@ -0,0 +1,24 @@
+#Variables explained
+#A variable is:
+#A named location...
+#In a computers memory...
+#it stores a value...
+#and the value can change
+
+#Example:
+name = "lebron"
+#The variable is called name
+#The value is "dave"
+#I will now change the value
+name = "james"
+
+#Variables can store different types of data
+#e.g. numbers, leters etc
+
+#The best programmers make sure the variable name is descriptive
+#e.g.
+TeamOneYellowCards = 0
+#The variable above has a very descriptive name
+x = 1
+#This variable has a poor name
+
