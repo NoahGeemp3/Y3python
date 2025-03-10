4576beb175d32f5593bba111a3c69b812139deff
SubaruPagani <7285@trinity.croydon.sch.uk>
2025-03-10 11:29:18 +0000

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,20 @@
+#DATA TYPES
+character = "d"
+realFloat = 12.34
+integer = 1234
+boolean = True
+string = "123abcdefg456"
+#C.R.I.B.S is a way to remember all the data types
+
+#You can "cast" for data types
+#for example:
+name = str(input("enter name"))
+#in this code str means we want the data entered as a string
+
+age = int(input("enter age"))
+#in this code int means we want the data entered as a integer
+
+price = float(input("enter price"))
+#in this code float means we want the data entered as a deciaml number
+
+#This is called casting for data types
